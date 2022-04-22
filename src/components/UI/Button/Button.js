a865fd57f4ddd400682f4f65458f08f1e2c42b35
santosh33 <santosh33@gmail.com>
2022-04-22 22:03:52 +0530

    CSS Modules with React

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,6 +1,9 @@
-import styled from "styled-components";
 
-const Button = styled.button`
+//import styled from "styled-components";
+
+import styles from './Button.module.css';
+
+/*const Button = styled.button`
     width:100%;
     font: inherit;
     padding: 0.5rem 1.5rem;
@@ -9,11 +12,11 @@ const Button = styled.button`
     background: #8b005d;
     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
     cursor: pointer;
-  
+
   @media (min-width: 768px) {
     width: auto;
   }
-  
+
 
   &:focus {
     outline: none;
@@ -26,14 +29,14 @@ const Button = styled.button`
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
 
-`;
+`;*/
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
